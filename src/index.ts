a12d8198cbfa325f8decbf63dc82663e642ea4c4
radian628 <70352880+radian628@users.noreply.github.com>
2022-09-24 22:48:26 -0700

    added home/tower select UI

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { getAllKeysDown, getMousePos, keycombo, rightMouseDown, setRightMouseDown, viewBottom, viewTopLeft } from "./controls.mjs";
+import { getAllKeysDown, getMousePos, keycombo, mousePosScreen, rightMouseDown, setRightMouseDown, viewBottom, viewTopLeft } from "./controls.mjs";
 import { getimg, drawTitle, drawGame } from "./draw.mjs";
 import { createDefaultEnemy, updateEnemies } from "./enemy_logic.mjs";
 import { enemyTextures, GameState, Screen, towerTextures } from "./game_state.mjs";
@@ -58,6 +58,67 @@ function isRepeatKeybind(keys: string[]) {
     return false;
 }
 
+function createTowerHomeUI(x: number, y: number) {
+    const root = document.createElement("div");
+    root.style.borderRadius = "50%";
+    root.style.background = "#00000088";
+    root.style.position = "absolute";
+    root.style.top = (y-50) + "px";
+    root.style.left = (x-50) + "px";
+    root.style.zIndex = "3";
+    root.style.width = "100px";
+    root.style.height = "100px";
+    root.style.display = "flex";
+    root.style.justifyItems = "center";
+    root.style.justifyContent = "center";
+    root.style.flexDirection = "column";
+    
+    root.focus();
+
+    root.onblur = function () {
+        document.body.removeChild(root);
+    }
+    root.onmouseleave = function () {
+        document.body.removeChild(root);
+    }
+
+    const homebtn = document.createElement("button");
+    homebtn.innerText = "Home"
+    root.appendChild(homebtn);
+    homebtn.style.height="20px";
+    homebtn.onclick = function () {
+        if (game.money >= game.homeCost) {
+            const mousepos = getMousePos();
+            game.homes.push({
+                x: mousepos.x,
+                y: mousepos.y,
+                hp: 1000,
+                maxHP: 1000
+            });
+            game.money -= game.homeCost;
+        }
+    }
+
+    const towerbtn = document.createElement("button");
+    towerbtn.innerText = "Tower";
+    root.appendChild(towerbtn);
+    towerbtn.style.height="20px";
+    towerbtn.onclick = function () {
+        if (
+            getAllKeysDown().length != 0 && !isRepeatKeybind(getAllKeysDown())
+        && getAllKeysDown().reduce((prev, curr) => prev && (curr.match(/^[0-9A-Z]$/g) !== null), true)
+        && game.money >= game.towerCost
+        ) {
+            //setRightMouseDown(false);
+            const mousepos = getMousePos();
+            game.towers.push(createDefaultTower(mousepos.x, mousepos.y, getAllKeysDown()));
+            game.money -= game.towerCost;
+        }
+    }
+
+    document.body.appendChild(root);
+}
+
 async function gameLoop() {
     if (!ctx) {
         window.alert("canvas error that should nver evr happen");
@@ -74,14 +135,9 @@ async function gameLoop() {
                 createDefaultEnemy(Math.random() * 3000 - 1500, Math.random() * 3000 - 1500),
             )
         }
-        if (rightMouseDown && getAllKeysDown().length != 0 && !isRepeatKeybind(getAllKeysDown())
-        && getAllKeysDown().reduce((prev, curr) => prev && (curr.match(/^[0-9A-Z]$/g) !== null), true)
-        && game.money >= game.towerCost
+        if (rightMouseDown
         ) {
-            setRightMouseDown(false);
-            const mousepos = getMousePos();
-            game.towers.push(createDefaultTower(mousepos.x, mousepos.y, getAllKeysDown()));
-            game.money -= game.towerCost;
+            createTowerHomeUI(mousePosScreen.x, mousePosScreen.y);
         }
         updateTowers(game);
         updateEnemies(game);
